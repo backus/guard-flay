d780132eafe1715710072e0f45adf2f6dc501f70
John Backus <jbackus@stanford.edu>
2015-07-15 20:59:30 -0700

    lame run_all

--- lib/guard/flay.rb
+++ lib/guard/flay.rb
@@ -11,7 +11,9 @@ module Guard
 
     def reload; end
 
-    def run_all; end
+    def run_all
+      Processor.process Dir['./**/*.rb']
+    end
 
     def run_on_changes(paths)
       Processor.process paths
